ead23637609cb4d6de8fbb7f8dfda6b2f31ecb04
dengwb <dengwenbin@wecash.net>
2020-02-03 11:09:16 +0800

    feat: add postcss-pxtorem

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,21 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+  plugins: [
+    require('postcss-pxtorem')({
+      rootValue: 75,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 12
+    })
+  ]
+  // "plugins": {
+  //   "postcss-import": {},
+  //   "postcss-url": {},
+  //   // to edit target browsers: use "browserslist" field in package.json
+  //   "autoprefixer": {}
+  // }
 }
